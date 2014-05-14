e8c3b6272ad8d1dc77ecd0620a9a5a73ed1f4ffd
elaine <chencuiping@zhenpin.com>
2014-05-14 09:13:51 +0800

    Initial Commit

---
+++ LittleGameIdeaTests/LittleGameIdeaTests.m
@@ -0,0 +1,34 @@
+//
+//  LittleGameIdeaTests.m
+//  LittleGameIdeaTests
+//
+//  Created by zp_liufei on 14-5-14.
+//  Copyright (c) 2014年 zp_liufei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LittleGameIdeaTests : XCTestCase
+
+@end
+
+@implementation LittleGameIdeaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
